020ffd11a323e33b5f97a07e9bb8fb9271491d0c
Hilkiah Makemo <hilmak01@gmail.com>
2018-09-11 11:48:43 -0600

    Removed admin/module from references

--- admin/Dashboard/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'admin', 'namespace' => 'Admin\Dashboard\Http\Controllers'], function()
-{
-    Route::resource('dashboard', DashboardController::class);
-});
