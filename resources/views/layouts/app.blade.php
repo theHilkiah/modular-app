68548fc361ad51bd012f9f54b00aba90d7fe8efc
Hilkiah Makemo <hilmak01@gmail.com>
2018-09-11 13:59:18 -0600

    Modified auth and updated views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Enroll') }}</a>
                             </li>
                         @else
                             <li class="nav-item dropdown">
